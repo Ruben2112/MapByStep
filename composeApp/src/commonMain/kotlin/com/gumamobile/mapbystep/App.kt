854c19a053eb78d2e847255a8eb72a8f920a2e14
Ruben2112 <ruben@vanamelsvoort.me>
2026-04-09 15:41:09 +0200

    Added initial styling values

--- composeApp/src/commonMain/kotlin/com/gumamobile/mapbystep/App.kt
+++ composeApp/src/commonMain/kotlin/com/gumamobile/mapbystep/App.kt
@@ -1,20 +1,13 @@
 package com.gumamobile.mapbystep
 
-import LocalSpacing
-import Spacing
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.tooling.preview.Preview
-import spacing
+import com.gumamobile.mapbystep.theme.MapByStepTheme
 
 @Composable
 @Preview
 fun App() {
-    CompositionLocalProvider(LocalSpacing provides Spacing()) {
-        MaterialTheme {
+    MapByStepTheme {
 
-        }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/com/gumamobile/mapbystep/theme/MapByStepTheme.kt
@@ -0,0 +1,33 @@
+package com.gumamobile.mapbystep.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+
+val LightColorTheme = lightColorScheme(
+    background = Background, // Activity background
+    surfaceContainer = SurfaceContainer, // Primary containers
+    surfaceContainerHigh = SurfaceContainerHigh, // Secondary containers (Toolbar, Nav bar, etc)
+    outline = Outline, // Outline for SurfaceContainer
+    onSurface = OnSurface, // Text and Icons on Surface
+    primaryContainer = PrimaryContainer, // Primary elements like buttons
+    onPrimaryContainer = OnPrimaryContainer, // Text and Icons on PrimaryContainer
+    outlineVariant = OutlineVariant, // Outline for PrimaryContainer
+    secondaryContainer = SecondaryContainer, // Secondary buttons like in Nav bar
+    onSecondaryContainer = OnSecondaryContainer, // Text and icons on SecondaryContainer
+)
+
+@Composable
+fun MapByStepTheme(
+    content: @Composable () -> Unit,
+) {
+    CompositionLocalProvider(LocalSpacing provides Spacing()) {
+        MaterialTheme(
+            colorScheme = LightColorTheme,
+            typography = Typography,
+            shapes = Shapes,
+            content = content,
+        )
+    }
+}
